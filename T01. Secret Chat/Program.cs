e602033c80178ad573d00d8bfcbeca69c2d896d2
Ivan.pamukliyski@students.softuni.bg <Ivan.pamukliyski@students.softuni.bg>
2021-11-27 15:13:56 +0200

    Add project files.

---
+++ T01. Secret Chat/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace T01._The_Imitation_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder message = new StringBuilder(input);
+            string command = Console.ReadLine();
+
+            while (command != "Decode")
+            {
+                var operation = command.Split("|");
+                if (operation[0] == "ChangeAll")
+                {
+                    message.Replace(operation[1], operation[2]);
+
+                }
+                else if (operation[0] =="Insert")
+                {
+                    int index = int.Parse(operation[1]);
+                    message.Insert(index, operation[2]);
+
+                }
+                else if (operation[0] == "Move")
+                {
+                    int index = int.Parse(operation[1]);
+                    string substring = message.ToString().Substring(0, index);
+                    message.Remove(0, index);
+                    message.Append(substring);
+                   
+                   
+                }
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"The decrypted message is: {message}");
+        }
+    }
+}
---
+++ T02. Destination Mapper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace T02._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Regex regex = new Regex(@"([=\/])([A-Z][A-Za-z]{2,})\1");
+            string input = Console.ReadLine();
+
+
+            MatchCollection matches = regex.Matches(input);
+            int index = 0;
+            int travelPoints = 0;
+            Console.Write("Destinations: ");
+            foreach (Match match in matches)
+            {
+                index++;
+                travelPoints += match.Groups[2].Length;
+                Console.Write($"{match.Groups[2].Value}");
+                if (index < matches.Count)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+            Console.Write($"Travel Points: {travelPoints}");
+
+
+
+
+
+        }
+    }
+}
